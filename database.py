5a93f3274337981aaa6d306b5d1222a0fbf8be31
Aremanda Abhijeeth <60499319+19PA1A0508@users.noreply.github.com>
2020-10-31 10:42:11 +0530

    Add files via upload

---
+++ database.py
@@ -0,0 +1,12 @@
+import random
+Names=["BBT SHELDON,","BBT RAJ KOOTHRAPALLI,","BBT LEONARD,","BBT HOWARD WOLOWITZ,","BBT AMY FRRAH FAWLER,","VERONICA,","UNQ GAMER,","I M RUTHLESS,","SOUL MORTAL,","SPOUL REGALTOS,","8BIT_REBEL,"]
+Id=random.randrange(564748369,595874846)
+RATIO=random.uniform(1,5)
+Tier=["CONQUERER","ACE","CROWN 1","CROWN 2","CROWN 3","CROWN 4","CROWN 5","DIAMOND 1","DIAMOND 2","DIAMOND 3","DIAMOND 4","DIAMOND 5","EVEN OUR BOTS HAVE GOOD TIER THAN YOU"]
+Server=["ASIA","MIDDLE EAST","KRJP","NORT AMERICA"]
+Win=random.randrange(10,30)
+Top_10=random.randrange(60,90)
+YOUR_NAME=random.choice(Names)
+Tier=random.choice(Tier)
+Server=random.choice(Server)
+print("NAME:",YOUR_NAME,"ID:",Id,",K/D RATIO:",RATIO,",CURRENT TIER:",Tier,",GAME SERVER:",Server,",NO OF WINS:",Win,",NO OF TOP 10'S:",Top_10)
